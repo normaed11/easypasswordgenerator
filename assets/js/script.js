38184f657e2098da27f8fa34ed6506d907ba4b6b
normadiaz <normadiazchb@me.com>
2022-01-13 18:49:28 -0600

    if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,12 +28,28 @@ function generatePassword() {
   var symbols = confirm("Include symbols?")
   console.log(passLength, number, lowercase, uppercase,
     symbols)
+  // creating individual arrays
+  var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  var uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*']
+  var passwordArray = []
+  if (number) {
+    passwordArray = numberArray.concat(passwordArray)
+  }
+  if (uppercase) {
+    passwordArray = uppercaseArray.concat(passwordArray)
+  }
+  if (lowercase) {
+    passwordArray = lowercaseArray.concat(passwordArray)
+  }
+  if (symbols) {
+    passwordArray = symbolsArray.concat(passwordArray)
+  }
 
-  var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  var lowercaseArray = 
-    
+  console.log(passwordArray)
+
 }
-
 
 
 
